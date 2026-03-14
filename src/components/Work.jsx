2748bea2ac88eb3ac29c89304ae0250f9d176604
Nuzzy32 <adrianfaishalhilmi@gmail.com>
2026-03-14 22:26:40 +0700

    pemanmbahan projek pertama

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,13 @@
 const works = [
-  { num: '/001', img: '/img/3.jpg', itemSpeed: -300, numSpeed: -1000 },
+  {
+    num: '/001',
+    img: '/img/riskala.png',
+    itemSpeed: -300,
+    numSpeed: -1000,
+    title: 'RISKALA Lite',
+    description: 'Developed RISKALA Lite, a full-stack mobile application (Flutter, Laravel, MySQL) focused on monitoring employee mental health. Engineered structured self-assessment tools (Likert-scale questionnaires) and an HR Admin Dashboard for real-time analytics, seamlessly integrated with an accessible, psychology-driven UI.',
+    link: 'https://github.com/Nuzzy32/riskala_lite2',
+  },
   { num: '/002', img: '/img/4.jpg', itemSpeed: -600, numSpeed: -500 },
   { num: '/003', img: '/img/5.jpg', itemSpeed: -700, numSpeed: -700 },
   { num: '/004', img: '/img/6.jpg', itemSpeed: -400, numSpeed: -200 },
@@ -15,14 +23,29 @@ export default function Work() {
         </h2>
       </div>
       <div className="work__wrapp">
-        {works.map((w) => (
-          <div key={w.num} className="work__item" data-speed={w.itemSpeed}>
-            <span className="work__item-num" data-speed={w.numSpeed}>{w.num}</span>
-            <div className="work__item-img">
-              <img src={w.img} alt={w.num} />
+        {works.map((w) => {
+          const inner = (
+            <div key={w.num} className="work__item" data-speed={w.itemSpeed}>
+              <span className="work__item-num" data-speed={w.numSpeed}>{w.num}</span>
+              <div className="work__item-img">
+                <img src={w.img} alt={w.title || w.num} />
+                {w.description && (
+                  <div className="work__item-overlay">
+                    <h3 className="work__item-overlay-title">{w.title}</h3>
+                    <p className="work__item-overlay-desc">{w.description}</p>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          )
+          return w.link ? (
+            <a key={w.num} href={w.link} target="_blank" rel="noopener noreferrer" className="work__item-link">
+              {inner}
+            </a>
+          ) : (
+            <div key={w.num}>{inner}</div>
+          )
+        })}
       </div>
     </section>
   )
--- src/hooks/useAnimations.js
+++ src/hooks/useAnimations.js
@@ -149,18 +149,6 @@ export default function useAnimations() {
       scrollTrigger: { trigger: '.contact', start: 'top bottom', scrub: 1.9 },
     })
 
-    // footer
-    gsap.from('.footer__div span', {
-      y: (i, el) => 1 - parseFloat(el.getAttribute('data-speed')),
-      opacity: 0,
-      scrollTrigger: {
-        trigger: '.footer',
-        start: 'top bottom',
-        end: 'bottom bottom',
-        scrub: 1.9,
-      },
-    })
-
     return () => {
       // cleanup all GSAP
       ScrollTrigger.getAll().forEach((t) => t.kill())
